def69dc20e8ff651ddf4441f7f174d2b05232393
Your Name <mann_9898771157@yahoo.com>
2020-06-26 17:06:39 +0530

    Initial commit

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -9,13 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    @State var showAlert = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color!")
+                }
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess * 255.0))"
+                        + " G: \(Int(gGuess * 255.0))"
+                        + " B: \(Int(bGuess * 255.0))")
+                }
+            }
+            Button(action: {self.showAlert = true}) {
+                Text("Hit me!")
+            }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Your score"), message: Text(String(computeScore())))
+            }.padding()
+            ColorSlider(value: $rGuess, textColor: .red)
+            ColorSlider(value: $gGuess, textColor: .green)
+            ColorSlider(value: $bGuess, textColor: .blue)
+        }
+    }
+    
+    func computeScore() -> Int {
+        let rDiff = rGuess - rTarget
+        let gDiff = gGuess - gTarget
+        let bDiff = bGuess - bTarget
+        let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
+        return Int((1.0 - diff) * 100.0 + 0.5)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.5, gGuess: 0.3, bGuess: 0.7).previewLayout(.fixed(width: 568, height: 320))
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255").foregroundColor(textColor)
+        }.padding(.horizontal)
     }
 }
